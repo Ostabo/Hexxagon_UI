164c827bd670fe20a83a4b98cf4d51eacfb5a97e
CI <CI@example.com>
2023-01-16 09:08:04 +0000

    deploy

---
+++ assets/api.6933fb8f.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index.aea1a63b.js";function o(t){return fetch(`${e}/`+t,{method:"POST",headers:{Accept:"application/json, text/plain, */*","Content-Type":"application/json"},body:""})}function a(t){return fetch(`${e}/`+t,{method:"GET",headers:{Accept:"application/json, text/plain, */*","Content-Type":"application/json"}})}export{o as a,a as b};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Hexxagon_UI/sw.js', { scope: '/Hexxagon_UI/' })})}
